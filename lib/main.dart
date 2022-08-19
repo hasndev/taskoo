c9ef7a61d93da340c3be6a48da0af342972b2e70
hasnja <hassanjabbaralfaraji@gmail.com>
2022-08-19 19:50:40 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Tutorial",
+      title: "Taskoo App",
       theme: ThemeData(primarySwatch: Colors.orange),
       debugShowCheckedModeBanner: false,
       home: OnboardingScreen(),
